f7ddab9a63e982e3b22602f4a6f73eb06a38bc07
0xKubitus <n.cubilier@gmail.com>
2022-04-23 23:45:09 +0200

    folder created

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# MY-TEMPLATE-REPO
+faire le point sur les gems dont j'ai besoin dans le fichier 'Gemfile' et dans lib/app.rb
+
+puis faire un 'bundle install' dans le terminal pour créer le 'Gemfile lock'
---
+++ spec/app_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../lib/app'
+
+describe "the ... method" do
+  it "should ..." do
+    expect(...(x)).to eq("instert expected result here")
+    
+  end
+
+  it "should ..." do
+    expect(...(x)).to eq("instert expected result here")
+    expect(...(x)).to eq("instert expected result here")
+  end
+end
+
+describe "the ... method" do
+  it "should ..." do
+    expect(...(x)).to eq("instert expected result here")
+  end
+end
+
